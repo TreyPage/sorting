ca7d823ccb3290cde304d9a722610446b4b65a8e
Terrell Page <tpage46@gmail.com>
2019-06-06 14:18:12 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Sorting.java
@@ -0,0 +1,40 @@
+package edu.cnm.deepdive;
+
+import java.util.Arrays;
+
+public class Sorting {
+
+  public static void sort(int[] data) {
+
+    if (data.length <= 1) {
+      return;
+    }
+
+    int[] left = Arrays.copyOf(data, data.length / 2);
+    int[] right = Arrays.copyOfRange(data, data.length / 2, data.length);
+
+    sort(left);
+    sort(right);
+
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int mergeIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        data[mergeIndex] = left[leftIndex];
+        leftIndex++;
+      } else {
+        data[mergeIndex] = right[rightIndex];
+        rightIndex++;
+      }
+      mergeIndex++;
+    }
+    if (leftIndex < left.length) {
+      System.arraycopy(left, leftIndex, data, mergeIndex, data.length - mergeIndex);
+    } else {
+      System.arraycopy(right, rightIndex, data, mergeIndex, data.length - mergeIndex);
+    }
+  }
+
+}
